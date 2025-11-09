67a09c1bfa86adb153eedcb79437dc6ce253e5a6
DJ Bostock <douglasjbostock@gmail.com>
2025-11-09 12:00:00 -0600

    cumulative update

---
+++ project/01-first-steps.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hey, this is my first .php file</h1>
+    <h2><?php echo 'Hello from PHP!'; ?></h2>
+    <h2><?php echo rand(1,6); ?></h2>
+</body>
+</html>
